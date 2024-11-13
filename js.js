351190cfa23c6326a5479f24dc48c6493e0d82f1
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2024-11-13 13:34:24 +0000

    update

--- js.js
+++ js.js
@@ -6,14 +6,23 @@ function loginButton() {
         alert("Please enter username and password");
         return;
     }
-    else if (username == "admin" && password == "admin") {
-        alert("Login successful");
-        window.location.href = "admin.html";
+    else if (username == "staff" && password == "staff") {
+        window.location.href = "staff.html";
     }
-    else if (username == "user" && password == "user") {
-        alert("Login successful");
-        window.location.href = "user.html";
+    else if (username == "shop" && password == "shop") {
+        window.location.href = "shop.html";
     }
+    else if (username == "manufacture" && password == "manufacture") {
+        window.location.href = "manufacture.html";
+    }
+    else if (username == "timetable" && password == "timetable") {
+        window.location.href = "timetable.html";
+    }
+    else if (username == "kermit" || password == "kermit") {
+        alert("bruh what? Kermit? Are you actually kidding me? no thats it im literally taking away your privileges");
+        alert("redirecting to youareanidiot.com");
+        alert("jk");
+    }   
     else {
         alert("Invalid username or password");
     }
