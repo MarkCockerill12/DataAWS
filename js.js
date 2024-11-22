68308bf3a9c52888c0f78ce2ab33ac73cec39c6f
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2024-11-22 10:58:42 +0000

    heh

--- js.js
+++ js.js
@@ -1,45 +1,3 @@
-function loginButton() {
-    console.log("loginButton");
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if (username == "" || password == "") {
-        alert("Please enter username and password");
-        return;
-    }
-    else if (password == "pass") {
-        if (username == "Customer") {
-            window.location.href = "Customer.html";
-        }
-        else if (username == "Staff") {
-            window.location.href = "Staff.html";
-        }
-        else if (username == "Manager") {
-            window.location.href = "Manager.html";
-        }
-        else if (username == "CEO") {
-            window.location.href = "CEO.html";
-        }
-        else if (username == "kermit" || password == "kermit") {
-            alert("bruh what? Kermit? Are you actually kidding me? no thats it im literally taking away your privileges");
-            alert("redirecting to youareanidiot.com");
-            alert("jk");
-        }   
-    }
-    else {
-        alert("Invalid username or password");
-        window.location.href = "image.jpg"
-
-    }
-
-    console.log(username);
-}
-
-
-
-
-
-
-
 // this updates the variable for whatever query type you select
 function loginButton() {
     console.log("loginButton");
@@ -70,6 +28,7 @@ function loginButton() {
     }
     else {
         alert("Invalid username or password");
+        window.location.href = "image.jpg";
     }
 
     console.log(username);
