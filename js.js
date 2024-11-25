35dd5783c5ed4be14f736c336b7f8d8fd4e0a7f0
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2024-11-25 11:00:38 +0000

    UPDATE

--- js.js
+++ js.js
@@ -1,5 +1,42 @@
 console.log("js.js script loaded");
 
+
+function loginButton() {
+    console.log("loginButton");
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "" || password == "") {
+        alert("Please enter username and password");
+        return;
+    }
+    else if (password == "pass") {
+        if (username == "Customer") {
+            window.location.href = "Customer.html";
+        }
+        else if (username == "Staff") {
+            window.location.href = "Staff.html";
+        }
+        else if (username == "Manager") {
+            window.location.href = "Manager.html";
+        }
+        else if (username == "CEO") {
+            window.location.href = "CEO.html";
+        }
+        else if (username == "kermit" || password == "kermit") {
+            alert("bruh what? Kermit? Are you actually kidding me? no thats it im literally taking away your privileges");
+            alert("redirecting to youareanidiot.com");
+            alert("jk");
+        }   
+    }
+    else {
+        alert("Invalid username or password");
+        window.location.href = "image.jpg";
+    }
+
+    console.log(username);
+}
+
+
 // Define global functions first
 function showHelpModal() {
     console.log("showHelpModal called");
@@ -21,9 +58,8 @@ function openTab(evt, tabName) {
     const tabDescription = document.getElementById('tabDescription');
     const tabImage = document.getElementById('tabImage');
 
-    tabTitle.style.display = 'block';
-    tabDescription.style.display = 'block';
-    tabImage.style.display = 'block';
+    // tabTitle.style.display = 'block';
+    // tabDe.style.display = 'block';
 
     if (tabName === 'SELECT') {
         tabTitle.textContent = 'SELECT Function';
