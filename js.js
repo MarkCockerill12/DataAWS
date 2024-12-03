7ce80dc1beba0a6b374eb98e719a4fcaee42c95a
L0lip <troytsang@gmail.com>
2024-12-03 20:46:52 +0000

    show password button

--- js.js
+++ js.js
@@ -36,6 +36,19 @@ function loginButton() {
     console.log(username);
 }
 
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('togglePasswordIcon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('bi-eye');
+        toggleIcon.classList.add('bi-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.remove('bi-eye-slash');
+        toggleIcon.classList.add('bi-eye');
+    }
+}
 
 // Define global functions first
 function showHelpModal() {
